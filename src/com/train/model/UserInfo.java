7a10c7e9c28bf3e33c3e3557be935af112bf8867
UUHsq <Administrator@EZ-20161011ZPEK>
2017-05-22 19:13:53 +0800

    Test git V1.4

--- src/com/train/model/UserInfo.java
+++ src/com/train/model/UserInfo.java
@@ -3,6 +3,9 @@ package com.train.model;
 public class UserInfo {
 	public static void main(String[] args) {
 		System.out.println("这仅仅是为了测试。");
+		System.out.println("测试更改后的效果。");
+		System.out.println("亦当无可无不可");
+		
 	}
 
 }
