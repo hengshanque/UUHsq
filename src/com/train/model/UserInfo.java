03bc0cd771ce6f085251309db60f2ba34d25d231
UUHsq <Administrator@EZ-20161011ZPEK>
2017-05-22 19:20:07 +0800

    v3.0

--- src/com/train/model/UserInfo.java
+++ src/com/train/model/UserInfo.java
@@ -1,6 +1,17 @@
 package com.train.model;
 
 public class UserInfo {
+	/**
+	 * 
+	 */
+	public void test() {
+		Integer mater=100;
+		String name="tome";
+		
+		System.out.println(name + mater);
+
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("这仅仅是为了测试。");
 		System.out.println("测试更改后的效果。");
